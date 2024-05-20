ab1dafe2eedd120c5af7e1c0dabf9dac4788fa86
Omar Carey <omarcarey@Omars-MacBook-Pro-2.local>
2024-05-20 18:22:30 +0800

    find lengths of messages

--- manytimepad/src/main.rs
+++ manytimepad/src/main.rs
@@ -6,9 +6,21 @@ fn main() {
 
     let messages = read_messages();
 
-    println!("{:?}", messages);
+    let lengths: Vec<usize> = messages.into_iter().map(|val| val.len()).collect();
+
+
+    println!("{:?}", lengths);
+
+    // for message in messages {
+    //     println!("{:?}", hex::decode(message).unwrap())
+    // }
+
 }
 
+
+fn find_length(messages: Vec<String>) {
+    let lengths: Vec<usize> = messages.into_iter().map(|val| val.len()).collect();
+}
 
 fn read_messages()-> Vec<String> {
     let mut file = File::open("messages.txt").unwrap();
