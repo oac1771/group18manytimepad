00c55f0175a1a79de77473e76c80c94ddb8d4980
jackht7 <jackhong.tey@gmail.com>
2024-05-26 13:14:59 +0800

    decrypt messages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use array_bytes::hex2bytes_unchecked;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -58,4 +60,55 @@ fn main() {
     }
 
     println!("longest chain: {chain_longest}");
+
+    let encrypted_messages = conversation
+        .clone()
+        .into_iter()
+        .map(|line| hex::decode(line).expect("Invalid hex in file"))
+        .collect::<Vec<Vec<u8>>>();
+
+    let decrypted_messages = decrypt_messages(&encrypted_messages);
+    for (i, messages) in decrypted_messages {
+        println!("Clue for message {}: {:?}", i + 1, messages);
+    }
+
+    let key = "Bitcoin: A purely peer-to-peer version of electronic cash would allow online payments to be sent directly from one party to another without going through a financial institution.";
+    for (i, ciphertext) in conversation.into_iter().enumerate() {
+        let message = decrypt(key.as_bytes(), ciphertext.as_str());
+        println!("Message {}: {:?}", i + 1, message);
+    }
+}
+
+fn decrypt_messages(encrypted_messages: &[Vec<u8>]) -> HashMap<usize, String> {
+    let mut decrypted_messages: HashMap<usize, String> = HashMap::new();
+
+    for i in 0..encrypted_messages.len() {
+        let mut decrypted_message = vec![95u8; encrypted_messages[i].len()];
+
+        for j in 0..encrypted_messages.len() {
+            if i != j {
+                let xor_result = xor_bytes(&encrypted_messages[i], &encrypted_messages[j]);
+
+                for (k, &byte) in xor_result.iter().enumerate() {
+                    if byte.is_ascii_alphabetic() || byte == b' ' {
+                        decrypted_message[k] = byte;
+                    }
+                }
+            }
+        }
+
+        let decrypted_string = String::from_utf8_lossy(&decrypted_message).to_string();
+        decrypted_messages.insert(i, decrypted_string);
+    }
+
+    decrypted_messages
+}
+
+fn xor_bytes(a: &[u8], b: &[u8]) -> Vec<u8> {
+    a.iter().zip(b.iter()).map(|(x, y)| x ^ y).collect()
+}
+
+pub fn decrypt(key: &[u8], cipher: &str) -> String {
+    let cipher = hex2bytes_unchecked(cipher);
+    String::from_utf8_lossy(&xor_bytes(key, &cipher)).to_string()
 }
